8ac5fcccbdc3dae14282e90a45d0ed9789a141de
pardeep5516 <43162246+pardeep5516@users.noreply.github.com>
2020-04-27 20:29:32 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:despicables_me_app/screen/character_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Despicable ME',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        canvasColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          elevation: 0.0,
+          color: Colors.white,
+        ),
+      ),
+      home: CharacterListingScreen(),
+    );
+  }
+}
